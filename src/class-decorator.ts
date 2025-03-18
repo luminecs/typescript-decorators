a622c571be14ba950fdcb4a6aeaaa331d96516a7
luminecs <wangxuke.china@gmail.com>
2025-03-18 11:12:43 +0800

    commit

---
+++ src/class-decorator.ts
@@ -0,0 +1,16 @@
+// 类装饰器应用于类声明，在类被定义时执行。
+
+// 定义一个简单的类装饰器
+function logClass(target: Function) {
+  console.log(`Class ${target} has been created`)
+}
+
+// 使用装饰器
+@logClass
+class Person {
+  name: string = 'Alice'
+}
+
+const person = new Person()
+
+// @logClass 是装饰器语法，target 是被装饰的类的构造函数。
